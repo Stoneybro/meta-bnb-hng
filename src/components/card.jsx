27cf50ab600f0690466064604fec68b049b42116
zion livingstone <zionlivingstone@gmail.com>
2022-11-16 02:32:18 +0100

    x

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const  Data = [
   {
-    img:'/src/assets/Frame 151 (4).png',
+    img:'../assets/Frame 151 (4).png',
   },
   {
     img:'../src/assets/Frame 151 (5).png',
